ff3b5f6fdcf6f3843a8930473cc5a91abf817ddb
NoahLu659 <36719883+NoahLu659@users.noreply.github.com>
2019-08-27 06:41:12 -0700

    items

--- js/Sub1.js
+++ js/Sub1.js
@@ -1,17 +1,28 @@
 var Sub1 = function(game) {
 	this.player;
-
+	this.mirror=false;
+	
 	this.border;
 	this.filter;
 	this.bounds;
 
 	this.door;
 	this.tables;
+
+	this.handMirror;
+	this.heart;
+	this.jar;
+	this.mandrake;
+
 };
 Sub1.prototype = {
 	preload: function() {
 		// Anything to preload during the Play state
 		console.log('Sub1: preload');
+		this.game.load.image('handMirror', 'assets/img/handMirrorIcon.png');
+		this.game.load.image('heart', 'assets/img/heartIcon.png');
+		this.game.load.image('jar', 'assets/img/jarIcon.png');
+		this.game.load.image('mandrake', 'assets/img/mandrakeIcon.png');
 	
 	},
 	create: function() {
@@ -21,6 +32,7 @@ Sub1.prototype = {
 		game.add.audio('snd_anxiety').play('', 0, 0.5, true);
 
 		/* --Objects & Furniture-- */
+		//bounds
 		this.bounds = game.add.group();
 		this.bounds.enableBody = true;
 		this.bounds.alpha = 0;
@@ -28,13 +40,29 @@ Sub1.prototype = {
 		this.bound.body.immovable = true;
 		this.bound = this.bounds.create(2210, 0, 'obj_bounds');
 		this.bound.body.immovable = true;
+		
+		//tables
 		this.tables = game.add.group();
 		this.tables.enableBody = true;
+		this.tables.create(600, 355, 'obj_table');
+		this.tables.create(800, 355, 'obj_table');
 		this.tables.create(1000, 355, 'obj_table');
-		this.tables.create(1750, 355, 'obj_table');
-		this.door = this.game.add.sprite(1200, 0, 'obj_door2');
-		game.physics.enable(this.door);
-
+		this.tables.create(1200, 355, 'obj_table');
+		//door
+		this.door = game.add.sprite(1600, 0, 'obj_door2');		
+		game.physics.enable(this.door);	
+		
+		//Items
+		this.handMirror = game.add.sprite(600, 340, 'handMirror');		
+		game.physics.enable(this.handMirror);
+		this.heart = game.add.sprite(800, 340, 'heart');		
+		game.physics.enable(this.heart);
+		this.jar = game.add.sprite(1000, 340, 'jar');		
+		game.physics.enable(this.jar);
+		this.mandrake = game.add.sprite(1200, 340, 'mandrake');		
+		game.physics.enable(this.mandrake);		
+		
+		//player
 		this.player = new Player(game, 200, game.world.height - 200, 'spr_player', controls);
 		this.game.add.existing(this.player);
 
@@ -51,21 +79,23 @@ Sub1.prototype = {
 	},
 	update: function() {
 		// Run the 'Play' state's game loop
-		
+		console.log(this.mirror);
 		/* --Collisions-- */
 		var isTouchingTable = game.physics.arcade.overlap(this.player, this.tables);
-		var touchedDoor = game.physics.arcade.collide(this.player, this.door);
+		var touchedDoor = game.physics.arcade.overlap(this.player, this.door);
+		var touchMirror = game.physics.arcade.overlap(this.player, this.handMirror);
 		game.physics.arcade.collide(this.player, this.bounds);
 
 
 		/* --Interaction-- */
-		if (isTouchingTable && controls.space.isDown) {
-			game.world.bringToTop(this.tables);
-			this.player.changeState('hidden');
-		}
 		if (touchedDoor&&controls.space.isDown) {
 			game.add.audio('snd_door').play('', 0, 0.05, false, false);
 			game.state.start('Level2');
+		}
+
+		if(touchMirror&&controls.space.isDown){
+			this.handMirror.alpha=0;
+			this.mirror=true;
 		}
 
 
@@ -77,6 +107,10 @@ Sub1.prototype = {
 		this.border.bringToTop();
 		this.filter.bringToTop();
 
+	},
+	render: function() {
+   	 	game.debug.body(this.handMirror);
+   	 	game.debug.body(this.player);
 	}
 		
 }

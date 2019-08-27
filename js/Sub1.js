fcd350a128dd47d6ad244d8f579ce7ecb19fc22c
NoahLu659 <36719883+NoahLu659@users.noreply.github.com>
2019-08-27 00:46:37 -0700

    Create Sub1.js

---
+++ js/Sub1.js
@@ -0,0 +1,13 @@
+var Sub1 = function(game) {};
+Sub1.prototype = {
+	preload: function() {
+		console.log('Sub1: preload');		
+	},
+	create: function() {
+		console.log('Sub1: create');
+		
+	}, 
+	update: function() {
+        
+	}
+}
